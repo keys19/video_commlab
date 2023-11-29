1e103785b13487dc5e111469a1790906c6fab9bf
Keya <120402183+keys19@users.noreply.github.com>
2023-11-30 01:13:26 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+         // Function to open the BTS popup gallery
+         function openBtsPopup() {
+            document.getElementById('bts-popup').style.display = 'flex';
+        }
+
+        // Function to close the BTS popup gallery
+        function closeBtsPopup() {
+            document.getElementById('bts-popup').style.display = 'none';
+        }
+
+         // Function to open the film description popup
+         function openFilmDescription() {
+            document.getElementById('film-description-popup').style.display = 'block';
+        }
+
+        // Function to close the film description popup
+        function closeFilmDescription() {
+            document.getElementById('film-description-popup').style.display = 'none';
+        }
